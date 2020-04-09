b740c7ed8688facf8dc3e7493b4f45ed97464adc
iN4sser <iN4sser@users.noreply.github.com>
2020-04-09 14:11:22 +0300

    Update IMDB-4ShowTV.user.js

--- IMDB-4ShowTV.user.js
+++ IMDB-4ShowTV.user.js
@@ -2,7 +2,7 @@
 // @author         iN4sser
 // @license        GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // @name           IMDb 4ShowTV
-// @version        0.1.0
+// @version        0.1.1
 // @description    Find IMDb Movies and TV Shows on 4ShowTV
 // @icon           *://www.4show.tv/favicon.ic
 // @updateURL      https://github.com/iN4sser/IMDB-4ShowTV/raw/master/IMDB-4ShowTV.user.js
@@ -97,6 +97,10 @@ if(div && title && year){
     //akoam
     img = "https://i.imgur.com/83uGmGM.png";
     buildCell(tr, "اكوام","https://akoam.net/search?q="+txt+" ", img);
+    
+    //mycima
+    img = "https://i.imgur.com/p7i0Zn7.png";
+    buildCell(tr, "ماي سيما","https://ww.mycima.co/search/"+txt+" ", img);
 }
 
 function buildCell(container, title, href, image){
