23810abd62f7ee3ef5fbe7a0e0dcacb139dcbbc8
iN4sser <iN4sser@users.noreply.github.com>
2020-04-22 00:26:32 +0300

    Update IMDB-4ShowTV.user.js
    updating some blocked urls

--- IMDB-4ShowTV.user.js
+++ IMDB-4ShowTV.user.js
@@ -2,7 +2,7 @@
 // @author         iN4sser
 // @license        GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // @name           IMDb 4ShowTV
-// @version        0.1.2
+// @version        0.1.3
 // @description    Find IMDb Movies and TV Shows on 4ShowTV
 // @icon           *://www.4show.tv/favicon.ic
 // @updateURL      https://github.com/iN4sser/IMDB-4ShowTV/raw/master/IMDB-4ShowTV.user.js
@@ -88,7 +88,7 @@ if(div && title && year){
   
     //Cima4u
     img = "https://i.imgur.com/dObEt33.png";
-    buildCell(tr, "سيما فور يو","http://w.cima4u.tv/?s="+txt+" ", img);
+    buildCell(tr, "سيما فور يو","https://cima4u.io/?s="+txt+" ", img);
   
     //Shahid4u
     img = "https://i.imgur.com/dgjhhSD.png";
@@ -100,7 +100,7 @@ if(div && title && year){
     
     //mycima
     img = "https://i.imgur.com/p7i0Zn7.png";
-    buildCell(tr, "ماي سيما","https://ww.mycima.co/search/"+txt+" ", img);
+    buildCell(tr, "ماي سيما","https://mycima.vip/search/"+txt+" ", img);
 }
 
 function buildCell(container, title, href, image){
