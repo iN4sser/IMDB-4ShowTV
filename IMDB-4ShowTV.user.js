b1cc2188438088ef5a5878cbb1e24c9dda0382d3
iN4sser <iN4sser@users.noreply.github.com>
2020-05-31 05:18:04 +0300

    Update IMDB-4ShowTV.user.js

--- IMDB-4ShowTV.user.js
+++ IMDB-4ShowTV.user.js
@@ -2,7 +2,7 @@
 // @author         iN4sser
 // @license        GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // @name           IMDb 4ShowTV
-// @version        0.1.8
+// @version        0.1.9
 // @description    Find IMDb Movies and TV Shows on 4ShowTV
 // @icon           *://www.4show.tv/favicon.ic
 // @updateURL      https://github.com/iN4sser/IMDB-4ShowTV/raw/master/IMDB-4ShowTV.user.js
@@ -113,6 +113,10 @@ if(div && title && year){
     //MovizLand
     img = "https://i.imgur.com/FSGbGxi.png";
     buildCell(tr, "موفيز لاند","https://tv1.movizland.com/search/"+txt+" ", img);
+    
+    //ArabTigers
+    img = "https://i.imgur.com/ekwdO6Y.png";
+    buildCell(tr, "عرب تايجرز","https://arabtigers.com/watch/?s="+txt+" ", img);
 }
 
 function buildCell(container, title, href, image){
