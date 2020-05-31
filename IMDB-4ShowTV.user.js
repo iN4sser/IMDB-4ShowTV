d76004e2dde5c1080bdd514c07c551c1fd626be8
iN4sser <iN4sser@users.noreply.github.com>
2020-05-31 05:33:54 +0300

    Update IMDB-4ShowTV.user.js

--- IMDB-4ShowTV.user.js
+++ IMDB-4ShowTV.user.js
@@ -108,7 +108,7 @@ if(div && title && year){
     
     //ArabSeed
     img = "https://i.imgur.com/NMdro5w.png";
-    buildCell(tr, "عرب سيد","https://arabseed.net/search?s="+txt+" ", img);
+    buildCell(tr, "عرب سيد","https://m2.arabseed.net/search?s="+txt+" ", img);
     
     //MovizLand
     img = "https://i.imgur.com/FSGbGxi.png";
