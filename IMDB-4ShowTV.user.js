7059e2af6d213fe6e967701bccdcda687f7c17f0
iN4sser <iN4sser@users.noreply.github.com>
2020-06-04 08:20:54 +0300

    Update IMDB-4ShowTV.user.js

--- IMDB-4ShowTV.user.js
+++ IMDB-4ShowTV.user.js
@@ -2,7 +2,7 @@
 // @author         iN4sser
 // @license        GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // @name           IMDb 4ShowTV
-// @version        0.2.0
+// @version        0.2.1
 // @description    Find IMDb Movies and TV Shows on 4ShowTV
 // @icon           *://www.4show.tv/favicon.ic
 // @updateURL      https://github.com/iN4sser/IMDB-4ShowTV/raw/master/IMDB-4ShowTV.user.js
@@ -121,6 +121,10 @@ if(div && title && year){
     //EgyHD
     img = "https://i.imgur.com/ZpdbzZ6.png";
     buildCell(tr, "EgyHD","https://egyhd.net/?s="+txt+" ", img);
+    
+    //Land4Movies
+    img = "https://i.imgur.com/KrAbsTV.png";
+    buildCell(tr, "Land4Movies","https://land4movies.tv/?s="+txt+" ", img);
 }
 
 function buildCell(container, title, href, image){
