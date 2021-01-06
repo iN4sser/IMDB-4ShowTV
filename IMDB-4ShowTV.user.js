ace0872cf02e02bd32c7f8d5e65903459bdd0be9
iN4sser <iN4sser@users.noreply.github.com>
2021-01-06 23:11:51 +0300

    Update IMDB-4ShowTV.user.js

--- IMDB-4ShowTV.user.js
+++ IMDB-4ShowTV.user.js
@@ -2,7 +2,7 @@
 // @author         iN4sser
 // @license        GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // @name           IMDb 4ShowTV
-// @version        0.2.7
+// @version        0.2.8
 // @description    Find IMDb Movies and TV Shows on 4ShowTV
 // @icon           *://www.4show.tv/favicon.ico
 // @updateURL      https://github.com/iN4sser/IMDB-4ShowTV/raw/master/IMDB-4ShowTV.user.js
@@ -129,6 +129,10 @@ if(div && title && year){
     //Land4Movies
     img = "https://i.imgur.com/KrAbsTV.png";
     buildCell(tr, "Land4Movies","https://land4movies.tv/?s="+txt+" ", img);
+    
+    //HonaDrama
+    img = "https://i.imgur.com/YYao00e.png";
+    buildCell(tr, "HonaDrama","https://honadrama.me/search/"+txt+" ", img);
 }
 
 function buildCell(container, title, href, image){
