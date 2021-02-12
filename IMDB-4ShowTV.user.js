17c083f9d60451a5b0a837a0dedcc37909603441
iN4sser <iN4sser@users.noreply.github.com>
2021-02-12 02:24:33 -0800

    Update IMDB-4ShowTV.user.js

--- IMDB-4ShowTV.user.js
+++ IMDB-4ShowTV.user.js
@@ -2,7 +2,7 @@
 // @author         iN4sser
 // @license        GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // @name           IMDb 4ShowTV
-// @version        0.2.8
+// @version        0.2.9
 // @description    Find IMDb Movies and TV Shows on 4ShowTV
 // @icon           *://www.4show.tv/favicon.ico
 // @updateURL      https://github.com/iN4sser/IMDB-4ShowTV/raw/master/IMDB-4ShowTV.user.js
@@ -108,7 +108,7 @@ if(div && title && year){
 
     //akoam
     img = "https://i.imgur.com/83uGmGM.png";
-    buildCell(tr, "اكوام","https://akwam.co/search?q="+txt+" ", img);
+    buildCell(tr, "اكوام","https://old.akwam.co/search/"+txt+" ", img);
     
     //MyEgy
     img = "https://i.imgur.com/9Nc2fH1.png";
