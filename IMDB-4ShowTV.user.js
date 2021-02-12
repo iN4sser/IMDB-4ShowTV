fc039fb209a7c9da8eb3fb79bf55773bcaa93dd8
iN4sser <iN4sser@users.noreply.github.com>
2021-02-12 02:31:08 -0800

    Update IMDB-4ShowTV.user.js

--- IMDB-4ShowTV.user.js
+++ IMDB-4ShowTV.user.js
@@ -2,7 +2,7 @@
 // @author         iN4sser
 // @license        GPL version 2 or any later version; http://www.gnu.org/licenses/gpl-2.0.txt
 // @name           IMDb 4ShowTV
-// @version        0.2.9
+// @version        0.3.0
 // @description    Find IMDb Movies and TV Shows on 4ShowTV
 // @icon           *://www.4show.tv/favicon.ico
 // @updateURL      https://github.com/iN4sser/IMDB-4ShowTV/raw/master/IMDB-4ShowTV.user.js
